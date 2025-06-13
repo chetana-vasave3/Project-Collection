72cf23574356719b2129429d5359dd3e58797801
chetana-vasave3 <chetanavasave3@gmail.com>
2025-06-13 15:23:26 +0530

    adding

--- app.py
+++ app.py
@@ -109,4 +109,5 @@ def add_student():
     return render_template('add_student.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
